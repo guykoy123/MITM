d8038644522029f011e09fa60148d6c6e13a65fa
Triceracop <guykoy123@gmail.com>
2017-11-15 16:00:17 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -2,17 +2,30 @@
 
 
 from scapy.all import *
+import subprocess
 
 # global variables:
 localHost=[]
+
+
+
+def proc_output(command):
+    """
+    returns output of command in the linux shell
+    """
+
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    (output, err) = proc.communicate()
+    return output
 
 def getLocalhostAddress():
     """
     returns default gateway address,
     Subnet Mask
     and localhost IP address"""
-    pass
-    #TODO: convert to linux with ifconfig|grep
+    defaultGateway=proc_output('ip route | awk \'/default/ { print $3 }\'')
+    localHost=proc_output('ip route | awk \'/src/ { print $9 }\'')
+    return defaultGateway,localHost
 
 def sendPacket(packet,defaultGateway):
     """
