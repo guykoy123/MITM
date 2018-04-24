ee958a9beb2f1d9e989365345ada53541e13f442
guykoy123 <guykoy123@gmail.com>
2018-04-24 11:51:42 +0300

    added sqlite3 database init file

--- MITM_make.sh
+++ MITM_make.sh
@@ -3,6 +3,3 @@ sudo apt-get install python-pip
 sudo pip install scapy
 sudo apt-get install arp-scan
 pip install flask
-sudo apt install python-pip
-sudo apt install unixodbc-dev
-sudo pip install pyodbc
---
+++ database/dbinit.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn =sqlite3.connect('test.db')
+
+conn.execute('''DROP TABLE IF EXISTS users;''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS users (
+  user_id INT PRIMARY KEY,
+  name TEXT NOT NULL,
+  password TEXT NOT NULL,
+  privilege INT);''')
+
+conn.execute('''DROP TABLE IF EXISTS sites;''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS sites (
+  url TEXT NOT NULL,
+  user_id INT,
+  FOREIGN KEY (user_id) REFERENCES users(user_id));''')
+
+conn.close()
